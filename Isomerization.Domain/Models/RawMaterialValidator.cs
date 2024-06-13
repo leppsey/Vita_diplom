b096bdc200f9bf03fdc1ff97ba7d58446a1fa118
romanKhlebnikov <khlebnikov.roman187@gmail.com>
2024-06-13 22:29:28 +0300

    s0m3 SUP3R ch4ng3s

--- Isomerization.Domain/Models/RawMaterialValidator.cs
+++ Isomerization.Domain/Models/RawMaterialValidator.cs
@@ -9,12 +9,6 @@ public class RawMaterialValidator: AbstractValidator<RawMaterial>
             .NotEmpty()
             .WithMessage("Поле не должно быть пустым");
         
-        RuleFor(c=>c.Consumption)
-            .NotEmpty()
-            .WithMessage("Поле не должно быть пустым")
-            .GreaterThan(0)
-            .WithMessage("Значение не может быть меньше нуля");
-        
         RuleFor(c=>c.Compound)
             .NotEmpty()
             .WithMessage("Поле не должно быть пустым")
@@ -28,18 +22,6 @@ public class RawMaterialValidator: AbstractValidator<RawMaterial>
             .WithMessage("Значение не может быть меньше нуля");
 
         RuleFor(c=>c.HeatCapacity)
-            .NotEmpty()
-            .WithMessage("Поле не должно быть пустым")
-            .GreaterThan(0)
-            .WithMessage("Значение не может быть меньше нуля");
-        
-        RuleFor(c=>c.Viscosity)
-            .NotEmpty()
-            .WithMessage("Поле не должно быть пустым")
-            .GreaterThan(0)
-            .WithMessage("Значение не может быть меньше нуля");
-        
-        RuleFor(c=>c.SulfurContent)
             .NotEmpty()
             .WithMessage("Поле не должно быть пустым")
             .GreaterThan(0)
